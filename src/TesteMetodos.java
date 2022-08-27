dff27d8a222f6accc5da8bf5763d2b07a7945a39
JessicaCrisSilva <jessicacristina278@gmail.com>
2022-08-27 20:40:17 -0300

    Initial commit

---
+++ src/TesteMetodos.java
@@ -0,0 +1,8 @@
+public class TesteMetodos {
+
+    public static void main(String[] args) {
+        Conta contadoPaulo = new Conta();
+        contadoPaulo.saldo = 100;
+        contadoPaulo.deposita(50);
+    }
+}
